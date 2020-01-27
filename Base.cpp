6a3c8629d263cc011edfdfb4b90dae9fb998f6b6
Harshit Maurya <hmaurya999@gmail.com>
2020-01-27 21:19:02 +0530

    Initialise cmake project

---
+++ Base.cpp
@@ -0,0 +1,5 @@
+//
+// Created by hmaurya on 25/01/20.
+//
+
+#include "Base.h"
---
+++ Base.h
@@ -0,0 +1,15 @@
+//
+// Created by hmaurya on 25/01/20.
+//
+
+#ifndef AUDIO_BASE_H
+#define AUDIO_BASE_H
+#include <iostream>
+#include <fstream>
+
+class Base {
+    
+};
+
+
+#endif //AUDIO_BASE_H
