807e15ec2e0757c8e776e9650485fe39113c2290
Harshit Maurya <hmaurya999@gmail.com>
2020-02-20 19:31:48 +0530

    generate sine tone

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <string>
 #include <utility>
+#include <math.h>
+
 using namespace std;
 
 // The default byte-write order is little-endian
@@ -256,6 +258,14 @@ public:
 
 int main() {
     WaveFileHandler wave_file("sample.wav");
+    int sample_rate = 44100;
+    int frequency = 500;
+    int duration = 5;
+    float amplitude = 0.1;
+    for (int i = 0; i <= sample_rate * duration; i++) {
+        short value = sin(2 * M_PI * i / sample_rate * frequency) * ((1 << 15) - 1) * amplitude;
+        wave_file.writeSample(value, 2);
+    }
     wave_file.close();
     return 0;
 }
